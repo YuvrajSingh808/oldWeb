8f398183a183ce679479eb31586bfef553be9bc8
Yuvraj Singh <yuvrajsingh808@gmail.com>
2022-12-20 19:47:33 +0530

    final changes

--- assets/js/toonsutra.js
+++ assets/js/toonsutra.js
@@ -4,20 +4,20 @@ document.getElementById("mail-form").addEventListener("submit", async (ev) => {
   try {
     spinner.style.display = "block";
     let email = document.getElementById("email");
-    let response = await fetch(
-      "https://devapi.toonsutra.com/api/v1.0/user/sendEnquiry",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          email: email.value.trim(),
-        }),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    let data = await response.json();
+    // let response = await fetch(
+    //   "https://devapi.toonsutra.com/api/v1.0/user/sendEnquiry",
+    //   {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       email: email.value.trim(),
+    //     }),
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+    // let data = await response.json();
     let emailData = await fetch(
       "https://ovk8yfmyxk.execute-api.us-east-1.amazonaws.com/Production",
       {
@@ -33,7 +33,7 @@ document.getElementById("mail-form").addEventListener("submit", async (ev) => {
     );
     console.log(await emailData.json());
     spinner.style.display = "none";
-    window.location.href = "./success.html";
+    window.location.href = "../../success.html";
   } catch (error) {
     spinner.style.display = "none";
     console.log(error);
